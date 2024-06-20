3b3bfc1005d174ec9d3a18c677ce7bcdcd8bbf98
Nitesh Kumar <niteshkr8953@gmail.com>
2024-06-20 18:03:59 +0530

    minor changes

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
 export const metadata: Metadata = {
-  title: 'YOOM',
+  title: 'Virtual-Connect',
   description: 'A workspace for your team, powered by Stream Chat and Clerk.',
 };
 
